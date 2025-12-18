f39fbf1929bf3108f9f4ef462b0bc8cf690068cd
seunghoonKang <tmdgns4321@gmail.com>
2025-12-19 04:50:14 +0900

    feat: SSR 서버 구현 및 fs 비동기 처리로 변경

--- packages/vanilla/server.js
+++ packages/vanilla/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "node:fs/promises";
 
 const prod = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 5173;
@@ -6,26 +7,51 @@ const base = process.env.BASE || (prod ? "/front_7th_chapter4-1/vanilla/" : "/")
 
 const app = express();
 
-const render = () => {
-  return `<div>안녕하세요</div>`;
-};
+let vite;
+if (!prod) {
+  const { createServer } = await import("vite");
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+  });
+  app.use(vite.middlewares);
+} else {
+  const compression = (await import("compression")).default;
+  const sirv = (await import("sirv")).default;
+  app.use(compression());
+  app.use(base, sirv("./dist/vanilla", { extensions: [] }));
+}
 
-app.get("*all", (req, res) => {
-  res.send(
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vanilla Javascript SSR</title>
-</head>
-<body>
-<div id="app">${render()}</div>
-</body>
-</html>
-  `.trim(),
-  );
+app.use("*all", async (req, res) => {
+  try {
+    const url = req.originalUrl;
+
+    let template;
+    let render;
+    if (!prod) {
+      template = await fs.readFile("./index.html", "utf-8");
+      template = await vite.transformIndexHtml(url, template);
+      render = (await vite.ssrLoadModule("/src/main-server.js")).render;
+    } else {
+      template = await fs.readFile("./dist/vanilla/index.html", "utf-8");
+      render = (await import("./dist/vanilla-ssr/main-server.js")).render;
+    }
+
+    const rendered = await render(url);
+
+    const html = template
+      .replace(
+        `<!--app-head-->`,
+        `${rendered.head ?? ""} ${rendered.data ? `<script>window.__INITIAL_DATA__ = ${JSON.stringify(rendered.data) ?? {}}</script>` : ""}`,
+      )
+      .replace(`<!--app-html-->`, rendered.html ?? "");
+
+    res.status(200).set({ "Content-Type": "text/html" }).send(html);
+  } catch (e) {
+    vite?.ssrFixStacktrace(e);
+    console.log(e.stack);
+    res.status(500).end(e.stack);
+  }
 });
 
 // Start http server
